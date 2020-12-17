b13b5dd221984bb632b948429dc42dc25ab186b2
zh <50803056+hhzule@users.noreply.github.com>
2020-12-17 20:35:15 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Header from "./Header"
+
+function App() {
+  const [mydata, setData] = useState([]);
+  const [todo, setTodo] = useState()
+  const [input, setInput] = useState()
+  const [isloading, setIsloading] = useState(true)
+  useEffect(() => {
+    (async () => {
+      setIsloading(true);
+      console.log("fetch called")
+      await fetch("/.netlify/functions/data")
+        .then(res => res.json())
+        .then(data => {
+          setData(data)
+        })
+      setIsloading(false)
+
+    })()
+  }, [todo])
+
+  console.log(mydata, "save")
+  const handleadd = (e, val) => {
+    e.preventDefault()
+    console.log("added")
+    console.log(val, "val")
+    if (val) {
+      console.log("right")
+      fetch(`/.netlify/functions/add`, {
+        method: 'POST',
+        body: JSON.stringify(val)
+      }).then(response => response.json())
+        .then(data => {
+
+          // console.log("Data: " + JSON.stringify(data.ref['@ref'].id));
+          console.log(data.ref['@ref'].id, "from add call")
+          setTodo(data);
+        });
+    } else {
+      alert("Task is required")
+    }
+
+  }
+  const handledelete = (id) => {
+    console.log(id, "from del")
+    fetch(`/.netlify/functions/deltask`, {
+      method: 'POST',
+      body: JSON.stringify(id)
+    }).then(response => response.json())
+      .then(data => {
+
+        // console.log("Data: " + JSON.stringify(data.ref['@ref'].id));
+        console.log(data.ref['@ref'].id, "from add call")
+        setTodo(data);
+      });
+  }
+
+  const handleupdate = (id, task) => {
+    console.log(id + task, "from update")
+    const updata = {
+      id,
+      task
+    }
+    fetch(`/.netlify/functions/update`, {
+      method: 'POST',
+      body: JSON.stringify(updata)
+    }).then(response => response.json())
+      .then(data => {
+
+        // console.log("Data: " + JSON.stringify(data.ref['@ref'].id));
+        console.log(data.ref['@ref'].id, "from add updata")
+        setTodo(data);
+      });
+  }
+  if (isloading)
+    return <h1>loading . . . </h1>
+
+  return (
+
+    <div className="App">
+      <Header />
+      <div className="primary"> <form>
+        <input value={input} onChange={(e) => setInput(e.target.value)}></input>
+        <button onClick={(e) => handleadd(e, input)}>add</button>
+      </form>
+        {
+          mydata?.map((d) => <div className="single-data"
+            key={d.ref['@ref'].id}>
+            <div className="heading">
+              <h2>
+                {JSON.stringify(d.data.task)}
+
+              </h2>
+            </div>
+
+            <div className="btns">
+              <button onClick={() => handledelete(d.ref['@ref'].id)}>delete</button>
+              <button onClick={() => handleupdate(d.ref['@ref'].id, "now")}>update</button>
+            </div>
+
+          </div>
+          )}
+      </div>
+
+
+
+
+
+    </div >
+  );
+}
+
+export default App;
---
+++ add.js
@@ -0,0 +1,30 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const faunadb = require('faunadb'),
+    q = faunadb.query;
+
+exports.handler = async (event, context) => {
+    // console.log(event.body)
+    try {
+        var client = new faunadb.Client({ secret: process.env.REACT_FAUNADB_SERVER_KEY });
+        var task = JSON.parse(event.body)
+        console.log(task, "task")
+        var result = await client.query(
+            q.Create(q.Collection('customers'), {
+                data: {
+                    task,
+                },
+            })
+        );
+        console.log("Document added Database: " + JSON.stringify(result));
+        // ok
+        return {
+            statusCode: 200,
+            body: JSON.stringify(result),
+
+        }
+    } catch (e) {
+        // something went wrong
+        console.log('Error: ');
+        console.log(e);
+    }
+}
